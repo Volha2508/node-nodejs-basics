dd2181a8a4940a46aa29422fd79a77519f1527cf
Olga Kapichenka <oshrub@gmail.com>
2024-01-25 16:56:23 +0100

    feat: delete.js done

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,8 @@
+import fs from 'fs';
 const remove = async () => {
-    // Write your code here 
+    fs.unlink('src/fs/files/fileToRemove.txt', (err) => {
+        if (err) throw ('FS operation failed'); 
+    });
 };
 
 await remove();
